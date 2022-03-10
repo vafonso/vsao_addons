800791e8d33f5a3dfa8e70bb9e17952ef68a2870
Vinicius Afonso <vinicius.afonso@hotmail.com>
2022-03-10 20:04:20 -0300

    commit

--- VSAO_POS/models/res_partner.py
+++ VSAO_POS/models/res_partner.py
@@ -4,5 +4,5 @@ from odoo import fields, models
 class ResPartner(models.Model):
     _inherit = 'res.partner'
     _description = 'Customer modification for PDV'
-    
-    vsao_POS_available = fields.Boolean(string="Mostra no PDV?")
+
+    vsao_POS_available = fields.Boolean(string='Mostra no PDV?')
