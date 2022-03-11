4707266030c67512e5717dacbb0343911fd76b15
Vinicius Afonso <vinicius.afonso@hotmail.com>
2022-03-11 16:11:25 -0300

    commit

--- VSAO_POS/models/res_config_settings.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
-    _description = 'Customer modification for PDV settings'
-
-    vsao_POS_filter_Partner = fields.Boolean(string='Mostrar somente Clientes do PDV?')
