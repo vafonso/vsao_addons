37e8d173c759ed87398f9a48f12114a0a33eead4
Vinicius Afonso <vinicius.afonso@hotmail.com>
2022-03-15 13:26:25 -0300

    commit

---
+++ vsao_todo/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'vsao_todo',
+    'version': '1.0',
+    'description': 'Tutorial App for Odoo Module creation learning',
+    'summary': 'Tutorial',
+    'author': 'Vinicius Carlos Afonso',
+    'website': '',
+    'license': 'LGPL-3',
+    'category': 'Tutorial',
+    'depends': ["base"
+        ''
+    ],
+    'data': [
+        ''
+    ],
+    'auto_install': False,
+    'application': True,
+    'assets': {
+        
+    }
+}
