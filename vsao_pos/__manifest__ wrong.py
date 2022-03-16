318be26d669b389a6a25252c08c552eeacb7a0d2
Vinicius Afonso <vinicius.afonso@hotmail.com>
2022-03-16 14:42:16 -0300

    commit

---
+++ vsao_pos/__manifest__ wrong.py
@@ -0,0 +1,15 @@
+# {
+#     'name': 'VSAO - Melhorias para PDV CM Semijoias',
+#     'version': '1.0',
+#     'description': 'Criação de pequenas melhorias para controle de visualização de registros dos atendentes do PDV',
+#     'summary': '1. Criação de UserGroup para visualização de registros próprios ou pessoa física'
+#                '2. Criação de UserGroup para visualização de todos os registros'
+#                '3. Inativar Record Rule res.partner.rule.private.employee',
+#     'author': 'Vinicius Carlos Afonso',
+#     'license': 'LGPL-3',
+#     'category': 'Generic Modules',
+#     'depends': ['base'],
+#     'data': ['security/vsao_pos.xml'],
+#     'auto_install': False,
+#     'application': False,
+# }
