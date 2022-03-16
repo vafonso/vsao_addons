3aec7733bc0ba67a780779ecdf2645214ae61c71
Vinicius Afonso <vinicius.afonso@hotmail.com>
2022-03-16 15:58:07 -0300

    commit

--- vsao_pos/__manifest__.py
+++ vsao_pos/__manifest__.py
@@ -4,7 +4,7 @@
     'description': 'Ocultar partes do sistema para usuário PDV',
     'author': 'Vinicius Carlos Afonso',
     'license': 'LGPL-3',
-    'depends': ['base'],
+    'depends': ['point_of_sale'],
     'data': ['views/vsao_pos.xml'],
     'auto_install': False,
     'application': False,
