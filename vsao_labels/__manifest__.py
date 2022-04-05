3ed14df6cd509db4a9aef66715b6bb169b543648
Vinicius Afonso <vinicius.afonso@hotmail.com>
2022-04-05 14:44:19 -0300

    commit

--- vsao_labels/__manifest__.py
+++ vsao_labels/__manifest__.py
@@ -5,6 +5,7 @@
     'author': 'Vinicius Carlos Afonso',
     'license': 'LGPL-3',
     'depends': ['product'],
+    'data': ['report/report.xml'],
     'auto_install': False,
     'application': False,
 }
