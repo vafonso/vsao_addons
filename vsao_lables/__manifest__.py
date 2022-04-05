8434ba377f391ecdb979f8576743b31de8a43409
Vinicius Afonso <vinicius.afonso@hotmail.com>
2022-04-05 14:27:32 -0300

    commit

---
+++ vsao_lables/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'vsao_lables',
+    'version': '1.0',
+    'description': 'Cria etiquetas ZPL',
+    'author': 'Vinicius Carlos Afonso',
+    'license': 'LGPL-3',
+    'depends': ['product'],
+    'auto_install': False,
+    'application': False,
+}
