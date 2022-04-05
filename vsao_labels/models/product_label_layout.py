5900b41b978157f5816d184d956175ee8667e13f
Vinicius Afonso <vinicius.afonso@hotmail.com>
2022-04-05 16:16:51 -0300

    commit

--- vsao_labels/models/product_label_layout.py
+++ vsao_labels/models/product_label_layout.py
@@ -8,3 +8,13 @@ class ProductLabelLayout(models.TransientModel):
         ('zplxsemijoia95x12xesquerda', 'ZPL Labels 95x12 Semijoia Esquerda'),
         ('zplxsemijoia95x12xdireita', 'ZPL Labels 95x12 Semijoia Direita')
     ], ondelete={'zplxsemijoia95x12xesquerda': 'set default', 'zplxsemijoia95x12xdireita': 'set default'})
+
+    def _prepare_report_data(self):
+        xml_id, data = super()._prepare_report_data()
+
+        if 'zplxsemijoia95x12xesquerda' in self.print_format:
+            xml_id = 'product.report_semijoia_95x12_esquerda'
+        elif 'zplxsemijoia95x12xdireita' in self.print_format:
+            xml_id = 'product.report_semijoia_95x12_direita'
+        
+        return xml_id, data
